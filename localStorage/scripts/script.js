cad0686eac5445c19def67e426bb35c46c2a02e8
jrgerodriguez <menjivar.jorge.93@gmail.com>
2024-02-25 15:23:08 -0600

    w7 complete

---
+++ localStorage/scripts/script.js
@@ -0,0 +1,53 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+let chaptersArray = getChapterList() || [];
+
+chaptersArray.forEach(chapter => {
+    displayList(chapter);
+});
+
+button.addEventListener('click', () => {
+
+    if (input.value != "") {
+        displayList(input.value);
+        chaptersArray.push(input.value);
+        setChapterList();
+        input.value = "";
+        input.focus();
+    }
+});
+
+function displayList(item) {
+    const li = document.createElement('li');
+    const deleteBtn = document.createElement('button');
+    li.textContent = item;
+    deleteBtn.textContent = "❌";
+    deleteBtn.classList.add('delete');
+    li.append(deleteBtn);
+    list.append(li);
+    input.value = "";
+    input.focus();
+
+    deleteBtn.addEventListener('click', () => {
+        list.removeChild(li);
+        deleteChapter(li.textContent);
+        input.value = "";
+        input.focus();
+    });
+}
+
+function setChapterList() {
+    localStorage.setItem('bomList', JSON.stringify(chaptersArray));
+}
+
+function getChapterList() {
+    return JSON.parse(localStorage.getItem('bomList'));
+}
+
+function deleteChapter(chapter) {
+    chapter = chapter.slice(0, chapter.length - 1);
+    chaptersArray = chaptersArray.filter((item) => item !== chapter);
+    setChapterList();
+}
