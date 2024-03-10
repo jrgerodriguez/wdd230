63970cad21d7a24dd39232ce00be0fb491171cba
jrgerodriguez <menjivar.jorge.93@gmail.com>
2024-03-09 23:59:42 -0600

    Collaboration Activity - JSON Menu complete

--- scripts/links.js
+++ scripts/links.js
@@ -1,10 +1,36 @@
 const baseURL = "https://jrgerodriguez.github.io/wdd230/";
 const linksURL = "https://jrgerodriguez.github.io/wdd230/data/links.json";
 
+const ulElement = document.querySelector('#links-list');
+
 async function getLinksData() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.log(data);
+    displayLinks(data);
+}
+
+const displayLinks = (weeks) => {
+    weeks.lessons.forEach((week) => {
+        let liElement = document.createElement('li');
+
+        liElement.textContent = `${week.lesson}: `;
+
+        week.links.forEach((link) => {
+            let aElement = document.createElement('a');
+            aElement.href = link.url;
+            aElement.textContent = link.title;
+            aElement.target = '_blank';
+            liElement.appendChild(aElement);
+            liElement.appendChild(document.createTextNode(' | '));
+        })
+
+        if (liElement.lastChild.nodeValue === ' | ') {
+            liElement.removeChild(liElement.lastChild);
+        }
+
+
+        ulElement.appendChild(liElement);
+    });
 }
 
 getLinksData();
