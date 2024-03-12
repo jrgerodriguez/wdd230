d87f0cc57e7c2f3ea1eacf3a21967fa75427090a
jrgerodriguez <menjivar.jorge.93@gmail.com>
2024-03-12 14:49:27 -0600

    direction in progress

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -8,7 +8,33 @@ const display = document.querySelector("article");
 async function getMembersData() {
     const response = await fetch(membersURL);
     const data = await response.json();
-    // displayLinks(data);
-    console.table(data)
+    displayMembers(data);
 }
 
+const displayMembers = (members) => {
+    members.members.forEach((member) => {
+        let section = document.createElement('section');
+        let img = document.createElement('img');
+        let name = document.createElement('h3');
+        let address = document.createElement('p');
+        let url = document.createElement('p');
+        let membership = document.createElement('p');
+
+        img.src = member.image;
+        name.textContent = member.name;
+        address.textContent = member.address;
+        url.textContent = member.url;
+        membership.textContent = `Memberhip Type: ${member.membership}`;
+
+        section.appendChild(img);
+        section.appendChild(name);
+        section.appendChild(address);
+        section.appendChild(url);
+        section.appendChild(membership);
+
+        display.appendChild(section);
+
+    })
+}
+
+getMembersData();
