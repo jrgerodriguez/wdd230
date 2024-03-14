d85a2dbd1067c5a5a61fef75855137318a90a3cc
jrgerodriguez <menjivar.jorge.93@gmail.com>
2024-03-13 18:58:21 -0600

    chamber complete

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,3 +1,22 @@
+// Last Modified
+
+let lastModif = new Date(document.lastModified);
+let dateString = new Date(lastModif).toLocaleString();
+document.querySelector('#lastModified').textContent = `Last update on ${dateString}`;
+
+// Hamburger menu
+
+const menuBtn = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+menuBtn.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	menuBtn.classList.toggle('open');
+});
+
+
+// Display Directory Members
+
 const baseURL = "https://jrgerodriguez.github.io/wdd230/";
 const membersURL = "https://jrgerodriguez.github.io/wdd230/chamber/data/members.json";
 
