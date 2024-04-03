a0f0d3837fdf7a4a3d5470169e8312f768301f19
jrgerodriguez <menjivar.jorge.93@gmail.com>
2024-04-03 01:46:31 -0600

    project in progress

--- final_website_project/scripts/script.js
+++ final_website_project/scripts/script.js
@@ -31,3 +31,99 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 });
+
+// ------------------------ WEATHER -------------------------
+
+// ------------------------ TEMPERATURE -------------------------
+
+const temperature = document.querySelector('#temperature');
+const tempIcon = document.querySelector('#temp-icon');
+const tempCaptionDesc = document.querySelector('#temp-figcaption');
+const tempMain = document.querySelector('#temp-main');
+
+const humidity = document.querySelector('#humidity');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=13.70&lon=-89.22&appid=f04c81d1f43f0c8dc412c6cf03b6a44b&units=metric';
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      displayResults(data);
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+apiFetch();
+
+function displayResults(data) {
+  temperature.innerHTML = `${Math.round(data.main.temp)}&deg;C`;
+  tempMain.textContent = `${data.weather[0].main}`;
+  const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+  let desc = data.weather[0].description;
+  tempIcon.setAttribute('src', iconsrc);
+  tempIcon.setAttribute('alt', desc);
+  tempCaptionDesc.textContent = `${capitalizeCaption(desc)}`;
+
+  humidity.innerHTML = `${data.main.humidity}%`;
+
+}
+
+function capitalizeCaption(phrase) {
+  let words = phrase.split(' '); //we separate the phrase in words by the space " "
+  for (let i = 0; i < words.length; i++) {
+    words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+  } // we iterate through every element (words) and capitalized the 0 character.
+  let capitalizedPhrase = words.join(" "); // then we put them all together
+
+  return capitalizedPhrase
+};
+
+// ------------------------ Next Day 3 pm -------------------------
+
+const temperatureNext = document.querySelector('#temperature-next');
+const iconNext = document.querySelector('#icon-next');
+const captionDescNext = document.querySelector('#figcaption-next');
+const MainNext = document.querySelector('#main-next');
+
+const forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=13.70&lon=-89.22&appid=f04c81d1f43f0c8dc412c6cf03b6a44b&units=metric';
+
+async function forecastApiFetch() {
+  try {
+    const response = await fetch(forecastUrl);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      displayForecastResults(data);
+    } else {
+      throw new Error(await response.text());
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+forecastApiFetch();
+
+function displayForecastResults(data) {
+  const tomorrow = new Date();
+  tomorrow.setDate(tomorrow.getDate() + 1);
+  tomorrow.setHours(9, 0, 0, 0);
+  const tomorrowUnixTimestamp = Math.floor(tomorrow.getTime() / 1000);
+
+  const forecast = data.list.find(item => item.dt === tomorrowUnixTimestamp);
+
+  temperatureNext.innerHTML = `${Math.round(forecast.main.temp)}&deg;C`;
+  MainNext.textContent = `${forecast.weather[0].main}`;
+  const forecastIcon = `https://openweathermap.org/img/w/${forecast.weather[0].icon}.png`;
+  let forecastDesc = forecast.weather[0].description;
+  iconNext.setAttribute('src', forecastIcon);
+  iconNext.setAttribute('alt', forecastDesc);
+  captionDescNext.textContent = `${capitalizeCaption(forecastDesc)}`;
+}
